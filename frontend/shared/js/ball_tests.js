acd31497b6a9667b69fdc87601dc20d81e9bdd42
Siphesihle Mhlongo <sihlecodes@gmail.com>
2024-07-01 21:58:56 +0200

    gyro test 15

--- frontend/shared/js/ball_tests.js
+++ frontend/shared/js/ball_tests.js
@@ -1,9 +1,10 @@
-import {clamp, round} from "./utils.js";
+import { Vector2 } from "./physics.js";
+import { clamp, round } from "./utils.js";
 
 const MAX_VELOCITY = 300;
 const MAX_ACCELARATION = 1000;
 
-let ball = { x: 0, y: 0, vx: 1, vy: 1, ax: 1, ay: 1 }
+let ball = { position: new Vector2(), velocity: new Vector2(), acceleration: new Vector2() }
 let previousTimeStamp = 0;
 
 // for debugging
@@ -13,30 +14,33 @@ function animate(timeStamp) {
     let canvas = document.getElementById("maze");
     let ctx = canvas.getContext("2d");
 
+    ctx.clearRect(ball.position.x - 12, ball.position.y - 12, 24, 24);
+
     const delta = (timeStamp - previousTimeStamp) / 1000.0;
-    previousTimeStamp = timeStamp;
     
-    ball.vx += ball.ax * MAX_ACCELARATION * delta;
-    ball.vy += ball.ay * MAX_ACCELARATION * delta;
+    ball.velocity.x += ball.acceleration.x * MAX_ACCELARATION * delta;
+    ball.velocity.y += ball.acceleration.y * MAX_ACCELARATION * delta;
 
-    ball.vx = clamp(ball.vx, -MAX_VELOCITY, MAX_VELOCITY);
-    ball.vy = clamp(ball.vy, -MAX_VELOCITY, MAX_VELOCITY);
+    ball.velocity.x = clamp(ball.velocity.x, -MAX_VELOCITY, MAX_VELOCITY);
+    ball.velocity.y = clamp(ball.velocity.y, -MAX_VELOCITY, MAX_VELOCITY);
     
-    vm = Math.sqrt(ball.vx * ball.vx + ball.vy * ball.vy);
+    let magnitude = Math.sqrt(ball.velocity.x * ball.velocity.x + ball.velocity.y * ball.velocity.y);
 
-    ball.x += MAX_VELOCITY * (ball.vx/vm) * delta;
-    ball.y += MAX_VELOCITY * (ball.vy/vm) * delta;
+    if (magnitude !== 0) {
+        ball.position.x += MAX_VELOCITY * (ball.velocity.x / magnitude) * delta;
+        ball.position.y += MAX_VELOCITY * (ball.velocity.y / magnitude) * delta;
+    }
 
-    ball.x = clamp(ball.x, 0, 500);
-    ball.y = clamp(ball.y, 0, 500);
+    ball.position.x = clamp(ball.position.x, 0, 500);
+    ball.position.y = clamp(ball.position.y, 0, 500);
     
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(ball.x, ball.y, 10, 0, 2 * Math.PI);
+    ctx.arc(ball.position.x, ball.position.y, 10, 0, 2 * Math.PI);
     ctx.stroke();
 
-    stats.innerText = `x: ${ball.x}, y: ${ball.y}, ax: ${ball.ax}, ay: ${ball.ay}, vx: ${ball.vx}, vy: ${ball.vy}`; 
+    stats.innerText = `x: ${ball.position.x}, y: ${ball.position.y}, acceleration.x: ${ball.acceleration.x}, acceleration.y: ${ball.acceleration.y}, velocity.x: ${ball.velocity.x}, velocity.y: ${ball.velocity.y}`; 
 
+    previousTimeStamp = timeStamp;
     window.requestAnimationFrame(animate);
 }
 
@@ -46,7 +50,7 @@ window.addEventListener("deviceorientation", (event) => {
     const y = round(event.beta / 180, 2);
     const z = round(event.gamma / 90, 2);
     
-    ball.ax = z;
-    ball.ay = y;
-    stats.innerText = `ax: ${z}, ay: ${y}`
+    ball.acceleration.x = z;
+    ball.acceleration.y = y;
+    // stats.innerText = `acceleration.x: ${z}, acceleration.y: ${y}`
 }, true);
--- frontend/shared/js/maze.js
+++ frontend/shared/js/maze.js
@@ -1,25 +1,117 @@
-const MAZE = [
-    [1, 1, 1, 1],
-    [1, 0, 0, 1],
-    [0, 0, 0, 0],
-    [1, 0, 0, 1],
-    [1, 0, 0, 1],
-    [1, 1, 0, 1],
-];
+import { BoundingBox, Vector2 } from "./physics.js";
 
-const ROWS = MAZE.length, COLUMNS = MAZE[0].length;
+let maze = {
+    data: [
+        [1, 1, 1, 1],
+        [1, 0, 0, 1],
+        [0, 0, 0, 0],
+        [1, 0, 0, 1],
+        [1, 0, 0, 1],
+        [1, 1, 0, 1],
+    ],
+    bounds: [],
+}
+
+maze.rows = maze.data.length;
+maze.columns = maze.data[0].length;
+
 const TILE_SIZE = 32;
+
+function generate_bounds(maze) {
+    let stack = [new Vector2(0, 0)];
+    let current = null;
+
+    while (stack.length > 0) {
+        if (current == null)
+            current = stack.pop();
+
+        if (!stack.length) {
+            if (maze.data[current.y + 1][current.x] && maze.data[current.y][current.x + 1]) {
+                stack.push(current);
+            }
+        }
+        
+    }
+}
+
+function recurse(maze, row, column, visited = [], bound = null, direction = null) {
+    if (row >= (maze.rows - 1) || column >= (maze.columns - 1)) {
+        if (bound) 
+            maze.bounds.push(bound);
+
+        return;
+    }
+
+    const current = new Vector2(row, column);
+
+    visited.forEach(element => {
+        if (element.equals(current))
+            return;
+    })
+
+    visited.push(current);
+
+    if (!bound && maze.data[row][column]) {
+        bound = new BoundingBox(
+            column * TILE_SIZE,
+            row * TILE_SIZE,
+            TILE_SIZE,
+            TILE_SIZE);
+    }
+
+    if (bound && direction) {
+        bound.width += direction.x * TILE_SIZE;
+        bound.height += direction.y * TILE_SIZE;
+    }
+
+    if (maze.data[row+1][column]) {
+        if (direction && !direction.equals(Vector2.DOWN)) {
+            maze.bounds.push(bound);
+            bound = null;
+            direction = null;
+        }
+        else if (!direction) {
+            direction = Vector2.DOWN;
+        }
+
+        recurse(maze, row + 1, column, visited, bound, direction);
+    }
+
+    if (maze.data[row][column+1]) {
+        if (direction && !direction.equals(Vector2.RIGHT)) {
+            maze.bounds.push(bound);
+            bound = null;
+            direction = null;
+        }
+        else if (!direction) {
+            direction = Vector2.RIGHT
+        }
+
+        recurse(maze, row, column + 1, visited, bound, direction);
+    }
+    // else {
+    //     if (bound)
+    //         maze.bounds.push(bound);
+
+    //     recurse(maze, row + 1, column, visited, null, null);
+    //     recurse(maze, row, column + 1, visited, null, null);
+    // }
+}
+
+recurse(maze, 0, 0)
+
+console.log(maze.bounds);
 
 function draw_maze(maze) {
     let canvas = document.getElementById("maze");
     let ctx = canvas.getContext("2d");
 
-    for (let i = 0; i < ROWS; i++) {
-        for (let j = 0; j < COLUMNS; j++) {
-            ctx.fillStyle = MAZE[i][j] ? "#58A8D8" : "transparent"; // Set fill color
+    for (let i = 0; i < maze.rows; i++) {
+        for (let j = 0; j < maze.columns; j++) {
+            ctx.fillStyle = maze.data[i][j] ? "#58A8D8" : "transparent";
             ctx.fillRect(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE);
         }
     }
 }
 
-draw_maze(MAZE);
+draw_maze(maze);
